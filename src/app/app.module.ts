f35ea3422086dae38b024a2b9c897f70a042806f
Lusenko <lusenko.sasha.2015@gmail.com>
2021-11-11 14:15:15 +0200

    feat: created side menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {SideMenuComponent} from "./side-menu/side-menu.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule

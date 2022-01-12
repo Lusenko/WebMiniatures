e5ab4315b5f21b70f71cccf3d0362d9423ad0bd5
Lusenko <lusenko.sasha.2015@gmail.com>
2022-01-12 13:29:46 +0200

    feat: added unsubscribe

--- src/app/main-catalogue/catalogues/main/main.component.ts
+++ src/app/main-catalogue/catalogues/main/main.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Option} from "./option";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {GetGameListService} from "../../../service/get-game-list.service";
 import {MatDialog} from "@angular/material/dialog";
-import {tap} from "rxjs/operators";
+import {takeUntil, tap} from "rxjs/operators";
 import {CreateCatalogueComponent} from "../create-catalogue/create-catalogue.component";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
   games$ = this.getGameListService.localGame$;
+  destroy$ = new Subject();
   option = Option
   formGroup: FormGroup;
 
@@ -23,14 +25,21 @@ export class MainComponent implements OnInit {
 
     this.formGroup = this.formBuilder.group({
       select: 'dateOfCreatingLf',
+      searchInput: ''
     })
   }
 
   ngOnInit(): void {
     this.formGroup.get('select')?.valueChanges.pipe(
-      tap(item => this.sorted(item))
+      tap(item => this.sorted(item)),
+      takeUntil(this.destroy$)
     ).subscribe();
 
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   delete(id: number){
@@ -50,19 +59,22 @@ export class MainComponent implements OnInit {
 
   sortByAZ(){
     this.getGameListService.localGame$.pipe(
-      tap(item => item.sort((a,b) => a.title.localeCompare(b.title)))
+      tap(item => item.sort((a,b) => a.title.localeCompare(b.title))),
+      takeUntil(this.destroy$)
     ).subscribe()
   }
 
   sortByZA(){
     this.getGameListService.localGame$.pipe(
-      tap(item => item.sort((a,b) => b.title.localeCompare(a.title)))
+      tap(item => item.sort((a,b) => b.title.localeCompare(a.title))),
+      takeUntil(this.destroy$)
     ).subscribe()
   }
 
   sortByAmount(){
     this.getGameListService.localGame$.pipe(
-      tap(item => item.sort((a,b) => a.quantity - b.quantity))
+      tap(item => item.sort((a,b) => a.quantity - b.quantity)),
+      takeUntil(this.destroy$)
     ).subscribe()
   }
 
@@ -73,7 +85,8 @@ export class MainComponent implements OnInit {
       panelClass: 'create-catalogue',
       width: '488px'
     }).afterClosed().pipe(
-      tap(item => this.getGameListService.addCatalogue(item))
+      tap(item => this.getGameListService.addCatalogue(item)),
+      takeUntil(this.destroy$)
     ).subscribe()
   }
 }

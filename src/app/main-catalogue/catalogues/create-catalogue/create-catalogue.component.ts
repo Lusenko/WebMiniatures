24271e8e7b8a623d057d1a49a7d588667537ff9f
Lusenko <lusenko.sasha.2015@gmail.com>
2022-02-09 14:01:56 +0200

    feat: added validation for description field

--- src/app/main-catalogue/catalogues/create-catalogue/create-catalogue.component.ts
+++ src/app/main-catalogue/catalogues/create-catalogue/create-catalogue.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-modal-dialog',
@@ -11,18 +11,28 @@ export class CreateCatalogueComponent implements OnInit {
 
   formGroup: FormGroup;
 
+  get InvalidLength()  {
+    return this.formGroup.get('description')?.errors?.invalidLength
+  }
+
   constructor(private readonly matDialogRef: MatDialogRef<CreateCatalogueComponent>, private readonly formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
       id: 0,
       images: ['assets/images/main/gameList/Rectangle1.svg'],
       title: '',
-      description: '',
+      description: ['', this.descriptionValid()],
       quantity: 0
     })
   }
 
   save(){
     this.matDialogRef.close(this.formGroup.value);
+  }
+
+  private descriptionValid(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null =>{
+      return  control.value.length > 299 ? { invalidLength: true} : null
+    }
   }
 
   ngOnInit(): void {
